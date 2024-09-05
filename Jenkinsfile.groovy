9cc7401f491d5617ae0939bf5ce18ba175bdc02a
andreas <andreas@devminds.ch>
2024-09-05 22:25:32 +0200

    Jenkins: fixup scripted pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -38,35 +38,36 @@ node {
             )
         }
         stage('Build project') {
-            sh 'cmake --preset default'
-            sh 'cmake --build --preset default --target cplusplus_training_project'
+            sh 'cmake --preset gcc-release'
+            sh 'cmake --build --preset gcc-release --target cplusplus_training_project'
             archiveArtifacts(
-                artifacts: 'build/default/bin/cplusplus_training_project',
+                artifacts: 'build/gcc-release/bin/cplusplus_training_project',
                 onlyIfSuccessful: true
             )
         }
         stage('Build tests') {
-            sh 'cmake --preset coverage'
-            sh 'cmake --build --preset coverage --target calculate_test'
+            sh 'cmake --preset gcc-coverage'
+            sh 'cmake --build --preset gcc-coverage --target calculate_test'
             archiveArtifacts(
-                artifacts: 'build/coverage/bin/calculate_test',
+                artifacts: 'build/gcc-coverage/bin/calculate_test',
                 onlyIfSuccessful: true
             )
         }
         stage('Run tests') {
-            sh './build/coverage/bin/calculate_test --gtest_output="xml:report.xml"'
+            sh './build/gcc-coverage/bin/calculate_test --gtest_output="xml:report-gcc.xml"'
             // Create Cobertura coverage report
-            sh 'gcovr -f src . --root ./ --exclude-unreachable-branches --xml-pretty --print-summary -o "coverage.xml"'
+            sh 'gcovr -f src . --root ./ --exclude-unreachable-branches --xml-pretty --print-summary -o "coverage-gcc.xml"'
             // Create HTML coverage report
             sh 'mkdir -p gcov'
             sh 'gcovr -f src . --root ./ --exclude-unreachable-branches --html --html-details -o "gcov/index.html"'
             junit(
-                testResults: 'report.xml'
+                testResults: 'report-gcc.xml'
             )
             recordCoverage(
+                name: 'GCC Coverage',
                 tools: [
-                    [parser: 'JUNIT', pattern: 'report.xml'],
-                    [parser: 'COBERTURA', pattern: 'coverage.xml']
+                    [parser: 'JUNIT', pattern: 'report-gcc.xml'],
+                    [parser: 'COBERTURA', pattern: 'coverage-gcc.xml']
                 ]
             )
             publishHTML([
