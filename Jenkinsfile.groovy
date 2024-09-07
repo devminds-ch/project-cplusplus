c7df3ee849ce5c879e2a3175de3d6899fa3f637c
andreas <andreas@devminds.ch>
2024-09-07 14:43:18 +0200

    Update scripted pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -37,49 +37,72 @@ node {
                 ]
             )
         }
-        stage('Build project') {
-            sh 'cmake --preset gcc-release'
-            sh 'cmake --build --preset gcc-release --target cplusplus_training_project'
-            archiveArtifacts(
-                artifacts: 'build/gcc-release/bin/cplusplus_training_project',
-                onlyIfSuccessful: true
-            )
+        def compilers = ["gcc", "clang"]
+        stage('Parallel build') {
+            def builds = [:]
+            compilers.each { c ->
+                builds[c] = {
+                    stage("Build project [${c}]") {
+                        sh "cmake --preset ${c}-release"
+                        sh "cmake --build --preset ${c}-release --target cplusplus_training_project"
+                        archiveArtifacts(
+                            artifacts: "build/${c}-release/bin/cplusplus_training_project",
+                            onlyIfSuccessful: true
+                        )
+                    }
+                    stage("Build tests [${c}]") {
+                        sh "cmake --preset ${c}-coverage"
+                        sh "cmake --build --preset ${c}-coverage --target calculate_test"
+                        archiveArtifacts(
+                            artifacts: "build/${c}-coverage/bin/calculate_test",
+                            onlyIfSuccessful: true
+                        )
+                    }
+                }
+            }
+            parallel builds
         }
-        stage('Build tests') {
-            sh 'cmake --preset gcc-coverage'
-            sh 'cmake --build --preset gcc-coverage --target calculate_test'
-            archiveArtifacts(
-                artifacts: 'build/gcc-coverage/bin/calculate_test',
-                onlyIfSuccessful: true
-            )
-        }
-        stage('Run tests') {
-            sh './build/gcc-coverage/bin/calculate_test --gtest_output="xml:report-gcc.xml"'
-            // Create Cobertura coverage report
-            sh 'gcovr -f src . --root ./ --exclude-unreachable-branches --xml-pretty --print-summary -o "coverage-gcc.xml"'
-            // Create HTML coverage report
-            sh 'mkdir -p gcov'
-            sh 'gcovr -f src . --root ./ --exclude-unreachable-branches --html --html-details -o "gcov/index.html"'
-            junit(
-                testResults: 'report-gcc.xml'
-            )
-            recordCoverage(
-                name: 'GCC Coverage',
-                tools: [
-                    [parser: 'JUNIT', pattern: 'report-gcc.xml'],
-                    [parser: 'COBERTURA', pattern: 'coverage-gcc.xml']
-                ]
-            )
-            publishHTML([
-                allowMissing: false,
-                alwaysLinkToLastBuild: false,
-                keepAll: false,
-                reportDir: 'gcov',
-                reportFiles: 'index.html',
-                reportName: 'Coverage',
-                reportTitles: '',
-                useWrapperFileDirectly: true
-            ])
+        stage('Parallel test execution')
+        {
+            def tests = [:]
+            compilers.each { c ->
+                tests[c] = {
+                    stage("Run tests [${c}]") {
+                        sh "./build/${c}-coverage/bin/calculate_test --gtest_output='xml:report-${c}.xml'"
+                        // Create Cobertura coverage report
+                        def gcov_executable = c == 'gcc' ? 'gcov' : 'llvm-cov gcov'
+                        sh "gcovr --gcov-executable '${gcov_executable}' -f src . --root ./ --exclude-unreachable-branches --xml-pretty --print-summary -o 'coverage-${c}.xml'"
+
+                        if (c == "gcc") {
+                            // Create HTML coverage report
+                            sh 'mkdir -p gcov'
+                            sh 'gcovr -f src . --root ./ --exclude-unreachable-branches --html --html-details -o "gcov/index.html"'
+                            publishHTML([
+                                allowMissing: false,
+                                alwaysLinkToLastBuild: false,
+                                keepAll: false,
+                                reportDir: 'gcov',
+                                reportFiles: 'index.html',
+                                reportName: 'Coverage',
+                                reportTitles: '',
+                                useWrapperFileDirectly: true
+                            ])
+                            junit(
+                                testResults: "report-${c}.xml"
+                            )
+                        }
+                        def coverage_name = c == 'gcc' ? 'GCC' : 'Clang'
+                        recordCoverage(
+                            name: "${coverage_name} Coverage",
+                            tools: [
+                                [parser: 'JUNIT', pattern: "report-${c}.xml"],
+                                [parser: 'COBERTURA', pattern: "coverage-${c}.xml"]
+                            ]
+                        )
+                    }
+                }
+            }
+            parallel tests
         }
     }
 }
