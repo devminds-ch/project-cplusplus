3e678e09dbd94be7b34738153c4976c43a351b71
Thomas <thomas@devminds.ch>
2024-10-17 11:51:08 +0200

    Add helper scripts to build projects
    Signed-off-by: Thomas <thomas@devminds.ch>

---
+++ tools/build-unit-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Save the current directory
+pushd .
+
+# Get Git root directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd "$REPO_ROOT" || exit 1
+
+# Build the project
+mkdir -p build
+cd build || exit 1
+cmake ..
+make calculate_test
+
+# Restore the directory
+popd || exit 1
+
