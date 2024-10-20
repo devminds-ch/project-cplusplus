5ae8486184dae14ac4910747015aeb14e70e0203
andreas <andreas@devminds.ch>
2024-10-20 09:12:56 +0000

    Major refactoring to use scripts for each step

--- tools/clang-format.sh
+++ tools/clang-format.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Get Git root directory
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-# Loop over files and format them
+pushd "${REPO_ROOT}" 2>&1 > /dev/null
+
 STATUS=0
+set +e
+
+echo "Running clang-format..."
 while read FILEPATH; do
     # Pass additional arguments to clang-format, e.g. --dry-run
     clang-format -i -style=file "$FILEPATH" --Werror $1
@@ -12,5 +16,8 @@ while read FILEPATH; do
         STATUS=1
     fi
 done <<< "$(find ${REPO_ROOT}/src ${REPO_ROOT}/tests \( -name '*.hpp' -o -name '*.cpp' \))"
+
+set -e
+popd 2>&1 > /dev/null
 
 exit $STATUS
