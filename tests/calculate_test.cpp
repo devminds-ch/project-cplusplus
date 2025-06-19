8a0de298a8a29ff27f5fc0d5e8a35afca053a02d
Andreas Nussberger <andreas@devminds.ch>
2025-06-19 18:00:04 +0200

    Add failing tests

--- tests/calculate_test.cpp
+++ tests/calculate_test.cpp
@@ -8,3 +8,15 @@ TEST(CalculateTest, Sum) {
     EXPECT_DOUBLE_EQ(3, Calculate::sum(2, 1));
     EXPECT_DOUBLE_EQ(4, Calculate::sum(2, 2));
 }
+
+TEST(CalculateTest, SumNegative) {
+    EXPECT_DOUBLE_EQ(0, Calculate::sum(-1, 1));
+    EXPECT_DOUBLE_EQ(0, Calculate::sum(1, -1));
+    EXPECT_DOUBLE_EQ(-2, Calculate::sum(-1, -1));
+}
+
+TEST(CalculateTest, SumFail) {
+    EXPECT_DOUBLE_EQ(3, Calculate::sum(1, 1));
+    EXPECT_DOUBLE_EQ(4, Calculate::sum(1, 2));
+    EXPECT_DOUBLE_EQ(4, Calculate::sum(2, 1));
+}
