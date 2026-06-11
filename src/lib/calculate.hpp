8276ea9f2d023065546c437a5d923f77bfdc9645
Thomas Keller <thomas@devminds.ch>
2026-06-11 13:46:31 +0200

    fix formatting issues
    Signed-off-by: Thomas Keller <thomas@devminds.ch>

--- src/lib/calculate.hpp
+++ src/lib/calculate.hpp
@@ -10,7 +10,7 @@ class Calculate {
      * @param b The second floating point number.
      * @return The sum of the two floating point numbers.
      */
-    static auto sum(const double &a, const double &b) -> double;
+    static auto sum(const double& a, const double& b) -> double;
 
     // TODO: add more functions here
 };
